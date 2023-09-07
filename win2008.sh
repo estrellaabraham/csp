10b081e05110ddf42f90f3fcad92ce1a1551424c
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-07 20:04:00 +0700

    Update win2008.sh

--- win2008.sh
+++ win2008.sh
@@ -2,7 +2,8 @@ apt-get update
 echo "Install QEMU"
 apt install qemu-kvm -y
 echo "Download windows files"
-wget -O win2012.img https://www.dropbox.com/scl/fi/aiojlzp2gxh5f0a6qjauf/windows-server-2012r2.img?rlkey=zxulkiq9inlb3xbffp152o5fd&dl=1
+wget -O Win2012.gz https://www.dropbox.com/scl/fi/kmri6yk1335vegf9019ub/Windows2012r2.gz?rlkey=nektutrzq7f5dhycqtfm6hbug&dl=1
+tar -xf Win2012.gz > /dev/null 2>&1
 echo "Download ngrok"
 wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 tar -xf ngrok.tgz > /dev/null 2>&1
@@ -20,6 +21,9 @@ echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
 nohup ./ngrok tcp --region $CRP 5900 &>/dev/null &
+echo "Starting Windows"
+qemu-system-x86_64 -hda windows-server-2012r2.img -m 4G -vnc :0 -smp cores=2  -net user -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga qxl -accel kvm &>/dev/null &
+clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
