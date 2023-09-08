4a71e6d008a0a01c8074549df9b776e6a24b79db
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-08 13:34:49 +0700

    Update win2008.sh

--- win2008.sh
+++ win2008.sh
@@ -2,8 +2,12 @@ apt-get update
 echo "Install QEMU"
 apt install qemu-kvm -y
 echo "Download windows files"
-wget -O Win2012.gz https://www.dropbox.com/scl/fi/kmri6yk1335vegf9019ub/Windows2012r2.gz?rlkey=nektutrzq7f5dhycqtfm6hbug&dl=1
-tar -xf Win2012.gz > /dev/null 2>&1
+wget -O win2008.iso https://www.dropbox.com/scl/fi/soc7cqxpsglbr8w53l992/W-server-2008-stan-data-enter-64-21AK22.iso?rlkey=yyzu49sg7mwics71f5ra2jks9&dl=1
+echo "Download bios64"
+wget -O bios64.bin https://github.com/BlankOn/ovmf-blobs/raw/master/bios64.bin
+echo "Create win2012.img"
+qemu-img create -f raw win2008.img
+qemu-img create -f raw win2008.img 20G
 echo "Download ngrok"
 wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 tar -xf ngrok.tgz > /dev/null 2>&1
@@ -20,21 +24,11 @@ echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
-nohup ./ngrok tcp --region $CRP 5900 &>/dev/null &
-echo "Starting Windows"
-qemu-system-x86_64 -hda windows-server-2012r2.img -m 4G -vnc :0 -smp cores=2  -net user -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga qxl -accel kvm &>/dev/null &
+./ngrok tcp --region $CRP 5900 &>/dev/null &
+sleep 1
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "===================================="
-echo "Username: akuh"
-echo "Password: Akuh.Net"
-echo "===================================="
-echo "===================================="
-echo "Keep supporting akuh.net, thank you"
-echo "You Got Free RDP now"
-echo "Wait 2 minute to finish bot"
-echo "You can close this tab"
-echo "RDP runs for 50 hours"
 echo "===================================="
 sleep 43200
