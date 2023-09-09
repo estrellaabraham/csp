d850b0680730cc7ef7afe001100f69cf6b55dd67
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-09 13:23:40 +0700

    Create win12.sh

---
+++ win12.sh
@@ -0,0 +1,31 @@
+apt-get update
+echo "Install QEMU"
+apt install qemu-kvm -y
+echo "Download windows files"
+wget -O win2012.img https://www.dropbox.com/scl/fi/cmjq4yvfgfi9htpzrhjft/win2012.img?rlkey=7ltwk2g532s8k2fi2behb2rv4&dl=1
+echo "Download ngrok"
+wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
+tar -xf ngrok.tgz > /dev/null 2>&1
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+clear
+echo "choose ngrok region (for better connection)."
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 5900 &>/dev/null &
+sleep 1
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+echo "Starting Windows"
+qemu-system-x86_64 -hda win2012.img -m 4G -vnc :0 -smp cores=2  -net user -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga qxl -enable-kvm -accel kvm &>/dev/null &
+clear
+echo RDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+sleep 43200
