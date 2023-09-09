e9d86c9df81ddd006a55d41cabf5809364c5c4e5
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-09 22:01:14 +0700

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -15,7 +15,7 @@ read -p "choose ngrok region: " CRP
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 echo "Starting Windows"
-qemu-system-x86_64 -hda win2012.img -m 4G -cpu host -net nic -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=2 -device rtl8139,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm &>/dev/null &
+qemu-system-x86_64 -hda win2012.img -m 4G -cpu host -vnc :0 -smp cores=2  -net user -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga qxl -enable-kvm -accel kvm &>/dev/null &
 clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
