99e79f9b270905c43ecf1abc415f891424f3cb04
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-09 13:40:58 +0700

    Update w2012.sh

--- w2012.sh
+++ w2012.sh
@@ -1,8 +1,3 @@
-apt-get update
-echo "Install QEMU"
-apt-get install qemu-system-x86 -y
-echo "Download windows files"
-wget -O w2012.img https://www.dropbox.com/scl/fi/aiojlzp2gxh5f0a6qjauf/windows-server-2012r2.img?rlkey=zxulkiq9inlb3xbffp152o5fd&dl=1
 echo "Download ngrok"
 wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 tar -xf ngrok.tgz > /dev/null 2>&1
@@ -20,19 +15,16 @@ echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
 nohup ./ngrok tcp --region $CRP 5900 &>/dev/null &
-echo "Starting Windows"
-qemu-system-x86_64 -hda w2012.img -m 4G -smp cores=4 -vnc :0 -smp cores=2 -net user -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
-echo RDP Address:
+echo Please wait for installing...
+sudo apt update -y > /dev/null 2>&1
+echo "Installing QEMU (2-3m)..."
+sudo apt install qemu-kvm -y > /dev/null 2>&1
+echo "Download windows files"
+curl -L -o win2012.img https://www.dropbox.com/scl/fi/cmjq4yvfgfi9htpzrhjft/win2012.img?rlkey=7ltwk2g532s8k2fi2behb2rv4&dl=1
+echo "Windows 2012 On Google Colab"
+echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "===================================="
-echo "Username: akuh"
-echo "Password: Akuh.Net"
-echo "===================================="
-echo "===================================="
-echo "Keep supporting akuh.net, thank you"
-echo "You Got Free RDP now"
-echo "Wait 2 minute to finish bot"
-echo "You can close this tab"
-echo "RDP runs for 50 hours"
-echo "===================================="
+echo "Note: Use Right-Click Or Ctrl+C To Copy"
+echo "Please Keep Colab Tab Open, Maximum Time 12h"
+qemu-system-x86_64 -hda w2012.img -m 4G -smp cores=4 -vnc :0 -smp cores=2 -net user -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
 sleep 43200
