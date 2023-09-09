7dd0a9c18e7017419b204ebcafb7159b44af6e1d
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-09 10:30:11 +0700

    Update lite10.sh

--- lite10.sh
+++ lite10.sh
@@ -26,6 +26,6 @@ echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
-sudo qemu-system-x86_64 -hda litexp.qcow2 -m 4G -vnc :0 -smp cores=2  -net user -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga qxl -enable-kvm -accel kvm &>/dev/null &
+sudo qemu-system-x86_64 -hda litexp.qcow2 -m 8G -vnc :0 -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
 sleep 43200
 
