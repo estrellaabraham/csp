6515f27f650be9131233121b64b487fdbefd19b2
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-10 13:32:46 +0700

    Update w7x68.sh

--- w7x68.sh
+++ w7x68.sh
@@ -1,7 +1,7 @@
 apt-get update
 read -p "Link win: " CRM
 echo "Download windows files"
-wget -O win2012.img $CRM
+wget -O win2012.qcow2 $CRM
 echo "Download ngrok"
 wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 tar -xf ngrok.tgz > /dev/null 2>&1
@@ -12,7 +12,7 @@ echo Downloading File From akuh.net
 apt-get install qemu-system-x86 -y
 echo "Wait"
 echo "Starting Windows"
-qemu-system-x86_64 -hda win2012.img -m 4G -smp cores=2 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
+qemu-system-x86_64 -hda win2012.qcow2 -m 4G -smp cores=2 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
 clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
