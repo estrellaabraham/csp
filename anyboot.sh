91845b4e549098b1846719844f25dbfa353df859
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-10 10:22:06 +0700

    Update and rename win2012.sh to anyboot.sh

--- anyboot.sh
+++ anyboot.sh
@@ -1,13 +1,14 @@
 apt-get update
 echo "Install QEMU"
 apt install qemu-kvm -y
+read -p "Link win: " CRM
 echo "Download windows files"
-wget -O win2012.iso https://bitly.ws/TZrM
+wget -O win2012.iso $CRM
 echo "Download bios64"
 wget -O bios64.bin https://github.com/BlankOn/ovmf-blobs/raw/master/bios64.bin
 echo "Create win2012.img"
 qemu-img create -f raw win2012.img
-qemu-img create -f raw win2012.img 30G
+qemu-img create -f raw win2012.img 20G
 echo "Download ngrok"
 wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 tar -xf ngrok.tgz > /dev/null 2>&1
