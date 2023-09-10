d2f91f85f66c00e8f35463a59c4d4e5c55a05dd2
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-10 10:05:30 +0700

    Update and rename vnc.sh to rsboot.sh

--- rsboot.sh
+++ rsboot.sh
@@ -11,7 +11,7 @@ echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
-screen ./ngrok tcp --region $CRP 5901 &>/dev/null &
+./ngrok tcp --region $CRP 5900 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 clear
