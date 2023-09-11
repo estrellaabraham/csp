f65b2640262756c0410aa2159b827b53224286f6
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-12 01:14:39 +0700

    Update machine.sh

--- machine.sh
+++ machine.sh
@@ -35,21 +35,22 @@ read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+echo "===================================="
+docker pull thuonghai2711/nomachine-ubuntu-desktop
+echo "===================================="
+docker run --rm -d -p 4000:4000 --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:xfce4
 clear
 echo "NoMachine: https://www.nomachine.com/download"
 echo Done! NoMachine Information:
-sudo apt update
-sudo apt install xfce4 -y
-wget -O nomachine.deb https://download.nomachine.com/download/8.8/Linux/nomachine_8.8.1_1_amd64.deb
-sudo apt install ./nomachine.deb
-clear
 echo IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
 echo User: user
 echo Passwd: 123456
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
 seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
-
-
+echo "===================================="
+docker pull frxyt/xrdp:lxde
+clear
+echo "===================================="
 
 
