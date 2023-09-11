9f2c76568466131935e66fbebdde86894c70e639
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-12 00:58:10 +0700

    Update machine.sh

--- machine.sh
+++ machine.sh
@@ -35,18 +35,21 @@ read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
-echo Downloading File From akuh.net
-sudo apt update && apt upgrade -y
+clear
+echo "NoMachine: https://www.nomachine.com/download"
+echo Done! NoMachine Information:
+sudo apt update
 sudo apt install xfce4 -y
 wget -O nomachine.deb https://download.nomachine.com/download/8.8/Linux/nomachine_8.8.1_1_amd64.deb
 sudo apt install ./nomachine.deb
 clear
-echo VPS Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "===================================="
-echo "Keep supporting akuh.net, thank you"
-echo "You Got Free VPS now"
-echo "Connect With Nomachine"
-echo "You can close this tab"
-echo "VPS runs for 50 hours"
-echo "===================================="
+echo IP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
+echo User: user
+echo Passwd: 123456
+echo "VM can't connect? Restart Cloud Shell then Re-run script."
+seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
+
+
+
+
