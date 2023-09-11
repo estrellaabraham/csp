462772888d5f404425437dccd3c1183b48c54e80
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-12 00:50:25 +0700

    Create machine.sh

---
+++ machine.sh
@@ -0,0 +1,52 @@
+wget -O ng.sh https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine/raw/main/ngrok.sh > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+
+
+function goto
+{
+    label=$1
+    cd 
+    cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
+          grep -v ':$')
+    eval "$cmd"
+    exit
+}
+
+: ngrok
+clear
+echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
+read -p "Paste Ngrok Authtoken: " CRP
+./ngrok authtoken $CRP 
+
+clear
+echo "Repo: https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine"
+echo "======================="
+echo "choose ngrok region (for better connection)."
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 4000 &>/dev/null &
+sleep 1
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+echo Downloading File From akuh.net
+sudo apt update && apt upgrade -y
+sudo apt install xfce4 -y
+wget -O nomachine.deb https://download.nomachine.com/download/8.8/Linux/nomachine_8.8.1_1_amd64.deb
+sudo apt install ./nomachine.deb
+clear
+echo VPS Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "Keep supporting akuh.net, thank you"
+echo "You Got Free VPS now"
+echo "Connect With Nomachine"
+echo "You can close this tab"
+echo "VPS runs for 50 hours"
+echo "===================================="
