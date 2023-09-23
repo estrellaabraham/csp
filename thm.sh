87fe9a6df6e980908975e66c23f33ed30a5f1035
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-23 21:16:08 +0700

    Update thm.sh

--- thm.sh
+++ thm.sh
@@ -35,62 +35,18 @@ read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
-
-sub-install-nomachine ()
-{
- echo ""
- echo ""
- echo "================================================================="
- echo " Install NoMachine v8.4.1 amd64  "
- echo "-----------------------------------------------------------------"
- read -p "Proceed ? (Y/n)" choice
- if [ "$choice" = "n" ]
-    then 
-        echo "Bypassing...." 
-    elif [ "$choice" = "N" ]
-	then
-       echo "Bypassing...." 
-    else 
-       echo "Running..."
-       sudo wget https://download.nomachine.com/download/8.4/Linux/nomachine_8.4.2_1_amd64.deb
-       sudo apt install -f ./nomachine_8.4.2_1_amd64.deb
-fi
-}
-####################################################
-
-sub-configue-nomachine-user ()
-{
- echo ""
- echo ""
- echo "================================================================="
- echo " Set up nomachine user & lock root user"
- echo "-----------------------------------------------------------------"
- read -p "Proceed ? (Y/n)" choice
- if [ "$choice" = "n" ]
-    then 
-        echo "Bypassing...." 
-    elif [ "$choice" = "N" ]
-	then
-       echo "Bypassing...." 
-    else 
-    echo "Running..."
-    sudo adduser nomachine
-         #(example password : paste  se7ye8pc5hs0  )
-    sudo usermod -aG sudo,adm,lp,sys,lpadmin nomachine
-    sudo passwd --delete --lock rootuser
-fi
-}
-run-cmd "sudo apt-get update" 
-run-cmd "sudo apt-get upgrade -y" 
-run-cmd "sudo apt-get install lxde"
-run-cmd "sudo apt-get install firefox -y"
-sub-install-nomachine
-sub-configue-nomachine-user
-run-cmd "sudo reboot"
+sudo apt update
+sudo apt upgrade
+sudo apt install lxde
+sudo wget https://download.nomachine.com/download/8.4/Linux/nomachine_8.4.2_1_amd64.deb
+sudo apt install -f ./nomachine_8.4.2_1_amd64.deb
+sudo adduser nomachine
+sudo usermod -aG sudo,adm,lp,sys,lpadmin nomachine
+sudo passwd --delete --lock rootuser
+echo "install brave"
 sudo apt install curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
 sudo apt install brave-browser
 clear
 echo "NoMachine: https://www.nomachine.com/download"
