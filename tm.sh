ff5ee91fb8f30ee7c15e5dfbb5d5f1812d348c73
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-25 00:50:50 +0700

    Update tm.sh

--- tm.sh
+++ tm.sh
@@ -45,21 +45,12 @@ sudo apt install firefox -y
 sudo apt install -y xrdp
 sudo apt install lxde -y
 sudo apt-get install -y lxde-terminal
-echo "================================"
-echo "Desktop Installation complete"
-echo "================================"
-echo "Install Brave"
-echo "==============="
-sudo apt install apt-transport-https curl > /dev/null 2>&1
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg > /dev/null 2>&1
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null 2>&1
-sudo apt update > /dev/null 2>&1
-sudo apt install brave-browser > /dev/null 2>&1
-echo "===================80%"
 sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh
 sudo service xrdp start
 clear
-echo "=======================100%"
+echo "================================"
+echo "Desktop Installation complete"
+echo "================================"
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
