217d436ce5ab89c19d9ce9250160f9e4042298a6
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-25 21:30:10 +0700

    Update gcs.sh

--- gcs.sh
+++ gcs.sh
@@ -6,7 +6,6 @@ wget -O ng.sh https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine/raw/ma
 chmod +x ng.sh
 ./ng.sh
 
-
 function goto
 {
     label=$1
@@ -22,18 +21,33 @@ clear
 echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
 read -p "Paste Ngrok Authtoken: " CRP
 ./ngrok authtoken $CRP 
-./ngrok tcp --region ap 3389 &>/dev/null &
+
+clear
+echo "Repo: https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine"
+echo "======================="
+echo "choose ngrok region (for better connection)."
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRM
+./ngrok tcp --region $CRM 3389 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+echo "===================================="
 echo "Install RDP"
 echo "===================================="
-docker pull danielguerra/alpine-xfce4-xrdp
+docker pull danielguerra/ubuntu-xrdp
 clear
 echo "===================================="
 echo "Start RDP"
 echo "===================================="
 echo "===================================="
-docker run -d --name rdp --shm-size 1g -p 3389:3389 danielguerra/alpine-xfce4-xrdp
+docker run -d --name uxrdp --hostname terminalserver --shm-size 1g -p 3389:3389 -p 2222:22 danielguerra/ubuntu-xrdp:16.04
 clear
 echo IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
