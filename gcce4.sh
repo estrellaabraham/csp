97366d5ace8541db8ff7987ba7f568e1eed28001
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-25 21:19:36 +0700

    Update gcce4.sh

--- gcce4.sh
+++ gcce4.sh
@@ -34,8 +34,8 @@ echo "au - Australia (Sydney)"
 echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CR
-./ngrok tcp --region $CR 3389 &>/dev/null &
+read -p "choose ngrok region: " CRM
+./ngrok tcp --region $CRM 3389 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 echo "===================================="
