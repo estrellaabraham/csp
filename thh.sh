44980cb7208ce2ffe67ea5c4afe8d5554d71163c
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-25 11:13:16 +0700

    Update thh.sh

--- thh.sh
+++ thh.sh
@@ -1,4 +1,3 @@
-sudo reboot
 wget -O ng.sh https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine/raw/main/ngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
@@ -33,16 +32,33 @@ echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 4000 &>/dev/null &
+./ngrok tcp --region $CRP 3389 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
-
+echo "===================================="
+echo "Install RDP"
+echo "===================================="
+docker pull danielguerra/ubuntu-xrdp
 clear
-echo "NoMachine: https://www.nomachine.com/download"
-echo Done! NoMachine Information:
-echo IP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
-echo User: user
-echo Passwd: 123456
-echo "VM can't connect? Restart Cloud Shell then Re-run script."
-seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
+echo "===================================="
+echo "Start RDP"
+echo "===================================="
+echo "===================================="
+echo "Username : ubuntu"
+echo "Password : ubuntu"
+echo "RDP Address:"
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "===================================="
+echo "Don't close this tab to keep RDP running"
+echo "Keep support akuh.net thank you"
+echo "Wait 1 minute to finish bot"
+echo "===================================="
+echo "===================================="
+docker run -d --name uxrdp --hostname terminalserver --shm-size 1g -p 3389:3389 -p 2222:22 danielguerra/ubuntu-xrdp:20.04 > /dev/null 2>&1
+echo XRDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "Don't close tab colab to keep rdp running 12 hours"
+echo "===================================="
+sleep 43200
