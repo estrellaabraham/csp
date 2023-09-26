88cd19924df0044b99cca31e8b20a4ad43222dc2
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-26 13:07:09 +0700

    Update gcce4.sh

--- gcce4.sh
+++ gcce4.sh
@@ -44,8 +44,8 @@ echo "===================================="
 sudo apt update && apt upgrade -y
 sudo apt install -y xrdp
 echo "===================================="
-sudo DEBIAN_FRONTEND=noninteractive \
-    apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
+read -p "Paste : " CR
+$CR
 echo "===================================="
 sudo apt-get install -y xfce4-terminal
 sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh
