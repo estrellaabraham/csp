1995dbbf99d47e70936ab522791557a86974b39d
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-27 14:03:40 +0700

    Update thh.sh

--- thh.sh
+++ thh.sh
@@ -36,7 +36,7 @@ echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 33890 &>/dev/null &
+./ngrok tcp --region $CRP 3389 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 echo "===================================="
