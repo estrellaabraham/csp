3ac5c09e87c0746b79dbbcbce26b842df908dabe
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-28 11:11:18 +0700

    Update egeh.sh

--- egeh.sh
+++ egeh.sh
@@ -1,9 +1,6 @@
-sudo apt install install curl
-
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+sudo rm microsoft.gpg
 sudo apt update
-
-sudo apt install install brave-browser
+sudo apt install microsoft-edge-dev
