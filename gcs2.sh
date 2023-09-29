b6d2a991315339a9550a5f9de800962539775533
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-29 14:05:30 +0700

    Update gcs2.sh

--- gcs2.sh
+++ gcs2.sh
@@ -43,7 +43,7 @@ echo "===================================="
 echo "Start RDP"
 echo "===================================="
 echo "===================================="
-docker run -d --network host --privileged --cap-add=SYS_PTRACE --shm-size=2g thuonghai2711/ubuntu22-vnc-pulseaudio:xfce4
+docker run -d --network host --shm-size 2g --privileged --cap-add=NET_ADMIN thuonghai2711/ubuntu22-vnc-pulseaudio:xfce4
 clear
 echo IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
