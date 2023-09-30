14aa04fe7d4f2b11c1b8659624e0d14f617f58e4
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-09-30 21:31:29 +0700

    Update gcs3.sh

--- gcs3.sh
+++ gcs3.sh
@@ -36,13 +36,13 @@ sleep 1
 echo "===================================="
 echo "Install RDP"
 echo "===================================="
-docker pull dorowu/ubuntu-desktop-lxde-vnc
+docker pull rsolano/ubuntu-vnc
 clear
 echo "===================================="
 echo "Start RDP"
 echo "===================================="
 echo "===================================="
-docker run -d --privileged --cap-add=SYS_PTRACE --shm-size=2g -p 6080:80 -p 5900:5900 -v /dev/shm:/dev/shm dorowu/ubuntu-desktop-lxde-vnc
+docker run --rm -p 5900:5900 --privileged --cap-add=SYS_PTRACE --shm-size=2g rsolano/ubuntu-vnc
 clear
 echo IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
