87a14ec8642aecbf587bd3f691e30c8941b1cdd1
estrellaabraham <141365903+estrellaabraham@users.noreply.github.com>
2023-10-06 19:30:18 +0700

    Update gcnm.sh

--- gcnm.sh
+++ gcnm.sh
@@ -35,7 +35,7 @@ echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRM
-./ngrok tcp --region $CRM 3389 &>/dev/null &
+./ngrok tcp --region $CRM 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 
